cb5976e96c4b8bdd00086280bbfcb3102e5b3253
romanm <roman.mishchenko@gmail.com>
2014-07-05 13:43:57 +0300

    Пошук пацієнта через angularjs в json файлі. Search patient through angularjs in json file.

---
+++ WebContent/db/patients.js
@@ -0,0 +1,11 @@
+patients = 
+[
+{name: 'Петренко',
+ diagnosis: 'Столбняк',
+ url : 'patient.html'
+ },
+{name: 'Іванчишин',
+ diagnosis: 'Лептоспироз™'},
+{name: 'Сидорчук',
+ diagnosis: 'Енцефаліт'}
+];
